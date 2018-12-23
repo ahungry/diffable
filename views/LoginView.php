c22be62c35dd9b4efcd06f6eea11ef76589ac748
Matthew Carter <m@ahungry.com>
2018-12-23 01:10:40 -0500

    Style a bit nicer

--- views/LoginView.php
+++ views/LoginView.php
@@ -1,21 +1,29 @@
 <?php require_once 'globals.php' ?>
 
-<form>
-  <?php s('error'); ?>
-  <input type="text"
-         value="<?php s('username'); ?>"
-         id="username"
-         diffable />
-  <?php s('info'); ?>
-  <input type="password"
-         value="<?php s('password'); ?>"
-         id="password"
-         diffable />
-  <button id="go"
-          clickable
-          diffable>Login</button>
-  <div>
-    <strong id="inc" clickable diffable>Clicked <?php s('counter'); ?> times!</strong>
-  </div>
-  <input type="hidden" id="counter" diffable value="<?php s('counter'); ?>" />
-</form>
+<div style="margin: auto;">
+  <form>
+    <?php s('error'); ?>
+    <div>
+      <input type="text"
+             value="<?php s('username'); ?>"
+             id="username"
+             diffable />
+    </div>
+    <?php s('info'); ?>
+    <div>
+      <input type="password"
+             value="<?php s('password'); ?>"
+             id="password"
+             diffable />
+    </div>
+    <div>
+      <button id="go"
+              clickable
+              diffable>Login</button>
+    </div>
+    <div>
+      <strong id="inc" clickable diffable>Clicked <?php s('counter'); ?> times!</strong>
+    </div>
+    <input type="hidden" id="counter" diffable value="<?php s('counter'); ?>" />
+  </form>
+</div>
