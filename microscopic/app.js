88ecfb67887b40ed14ef784d399cb017ef9575b1
Matthew Carter <m@ahungry.com>
2019-04-22 01:42:45 -0400

    Good stuff

--- microscopic/app.js
+++ microscopic/app.js
@@ -1,4 +1,18 @@
 // A fake (minimal) React
+var idc = 0; // id counter
+function makeElement(element) {
+    idc++;
+    var node = document.createElement(element);
+    node.id = idc;
+    return node;
+}
+function renderCustom(element) {
+    var node = makeElement(element.constructor.name);
+    node.onclick = element.onClick.bind(node);
+    node.appendChild(element.render());
+    element.props.id = node.id;
+    return node;
+}
 var React = {
     createElement: function (tag, attrs, children) {
         var _a;
@@ -7,25 +21,28 @@ var React = {
         switch (typeof tag) {
             case 'function':
                 var t = (_a = {}, _a[tag] = tag, _a);
-                element = (new t[tag]());
+                element = (new t[tag](attrs));
                 element.props = {
                     children: document.createElement('span')
                 };
                 isFn = true;
+                console.log(element.constructor.name);
                 break;
             case 'string':
             default:
                 element = document.createElement(tag);
                 break;
         }
-        for (var name_1 in attrs) {
-            if (name_1 && attrs.hasOwnProperty(name_1)) {
-                var value = attrs[name_1];
-                if (value === true) {
-                    element.setAttribute(name_1, name_1);
-                }
-                else if (value !== false && value != null) {
-                    element.setAttribute(name_1, value.toString());
+        if (false === isFn) {
+            for (var name_1 in attrs) {
+                if (name_1 && attrs.hasOwnProperty(name_1)) {
+                    var value = attrs[name_1];
+                    if (value === true) {
+                        element.setAttribute(name_1, name_1);
+                    }
+                    else if (value !== false && value != null) {
+                        element.setAttribute(name_1, value.toString());
+                    }
                 }
             }
         }
@@ -40,22 +57,41 @@ var React = {
                     document.createTextNode(child.toString()) : child);
             }
         }
-        return isFn ? element.render() : element;
+        return isFn ? renderCustom(element) : element;
     }
 };
 var Hello = /** @class */ (function () {
-    function Hello() {
+    function Hello(props) {
+        var _this = this;
+        this.clicked = 0;
+        this.onClick = function () {
+            _this.clicked++;
+            console.log(_this.clicked);
+            _this.setState();
+        };
+        // Just a redraw for now
+        this.setState = function () {
+            console.log(_this.props.id);
+            var self = React.createElement(Hello, { clicked: _this.clicked });
+            var t = document.getElementById(_this.props.id);
+            t.parentNode.replaceChild(self, t);
+        };
+        this.clicked = props.clicked || 0;
     }
     Hello.prototype.render = function () {
         return (React.createElement("div", null,
             "Greetings to you!",
-            React.createElement("strong", null, this.props.children)));
+            React.createElement("strong", null, this.props.children),
+            React.createElement("b", null,
+                "Clicked ",
+                this.clicked,
+                " times!")));
     };
     return Hello;
 }());
 var title = "Hello World";
 document.getElementById('app').appendChild(React.createElement("div", null,
-    React.createElement(Hello, null, "Greetings!"),
+    React.createElement(Hello, { clicked: 0 }, "Greetings!"),
     React.createElement("h1", null, title),
     React.createElement("h2", null, "This is a template written in TSX, then compiled to JSX by tsc (the Typescript compiler), and finally injected into a web page using a script")));
 // var hello = <Hello clicked={0} name="Matt" />
--- microscopic/app.tsx
+++ microscopic/app.tsx
@@ -1,4 +1,23 @@
 // A fake (minimal) React
+var idc = 0 // id counter
+
+function makeElement (element) {
+  idc++
+  var node = document.createElement(element)
+  node.id = idc
+
+  return node
+}
+
+function renderCustom (element) {
+  var node = makeElement(element.constructor.name)
+  node.onclick = element.onClick.bind(node)
+  node.appendChild(element.render())
+  element.props.id = node.id
+
+  return node
+}
+
 const React = {
   createElement: function (tag, attrs, children) {
     var isFn = false
@@ -7,11 +26,12 @@ const React = {
     switch (typeof tag) {
       case 'function':
         const t = { [tag]: tag }
-        element = (new t[tag]())
+        element = (new t[tag](attrs))
         element.props = {
           children: document.createElement('span')
         }
         isFn = true
+        console.log(element.constructor.name)
         break
 
       case 'string':
@@ -20,13 +40,15 @@ const React = {
         break
     }
 
-    for (let name in attrs) {
-      if (name && attrs.hasOwnProperty(name)) {
-        let value = attrs[name]
-        if (value === true) {
-          element.setAttribute(name, name)
-        } else if (value !== false && value != null) {
-          element.setAttribute(name, value.toString())
+    if (false === isFn) {
+      for (let name in attrs) {
+        if (name && attrs.hasOwnProperty(name)) {
+          let value = attrs[name]
+          if (value === true) {
+            element.setAttribute(name, name)
+          } else if (value !== false && value != null) {
+            element.setAttribute(name, value.toString())
+          }
         }
       }
     }
@@ -43,16 +65,37 @@ const React = {
             document.createTextNode(child.toString()) : child)
       }
     }
-    return isFn ? element.render() : element
+    return isFn ? renderCustom(element) : element
   }
 }
 
 class Hello {
   public props
+  public clicked = 0
+
+  constructor (props) {
+    this.clicked = props.clicked || 0
+  }
+
+  public onClick = () => {
+    this.clicked++
+    console.log(this.clicked)
+    this.setState()
+  }
+
+  // Just a redraw for now
+  setState = () => {
+    console.log(this.props.id)
+    var self = <Hello clicked={this.clicked} />
+    var t = document.getElementById(this.props.id)
+    t.parentNode.replaceChild(self, t)
+  }
 
   render () {
     return (
-      <div>Greetings to you!<strong>{this.props.children}</strong></div>
+      <div>Greetings to you!<strong>{this.props.children}</strong>
+        <b>Clicked {this.clicked} times!</b>
+      </div>
     )
   }
 }
@@ -61,7 +104,7 @@ const title = "Hello World"
 
 document.getElementById('app').appendChild(
   <div>
-    <Hello>Greetings!</Hello>
+    <Hello clicked={0}>Greetings!</Hello>
     <h1>{title}</h1>
     <h2>This is a template written in TSX, then compiled to JSX by tsc (the Typescript compiler), and finally
             injected into a web page using a script</h2>
